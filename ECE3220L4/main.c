7507516e28c1de9ee88e35905d1ae50cc1d1ad1c
Tyler Shipman <TylerShipman@mwc-065037.dhcp.missouri.edu>
2016-09-22 10:12:29 -0500

    L4 v1.0

---
+++ ECE3220L4/main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: TylerShipman
+ *
+ * Created on September 22, 2016, 10:08 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
